4cfbf965aee035d419812ed7230d4dade8d34931
小野和論 <matopenkw@gmail.com>
2020-01-16 00:33:58 +0900

    init

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.Static("/assets", "./assets")
+
+	router.LoadHTMLGlob("templates/*.html")
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "index.html", gin.H{})
+	})
+
+	router.Run()
+}
