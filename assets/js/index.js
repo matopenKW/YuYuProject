70f125eb4fb8ec9a82a9d4116f0c8e657430cc67
matopenKW <matopenkw@gmail.com>
2020-01-19 01:07:35 +0900

    Add svg

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,8 @@
 $(function(){
     $(document).on('click', '.floor', function(){
 
-        
-
         $('#modal .modal-title').html($(this).html());
-        $('#modal .modal-body').html('12234');
+        // $('#modal .modal-body').html('12234');
 
         $('#modal').modal('show');
 
