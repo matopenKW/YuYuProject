77e94c173bd1b0a41d87ea3f3104d7b1c21bb759
小野和論 <matopenkw@gmail.com>
2020-01-27 18:59:50 +0900

    Update main.go json errorhandring

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"YuYuProject/internal/dto"
+	"encoding/json"
+	"github.com/JustinTulloss/firebase"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"log"
 )
 
 type Team struct {
@@ -18,16 +22,17 @@ func main() {
 	router.LoadHTMLGlob("templates/*.html")
 
 	router.GET("/", show("index"))
-	router.GET("/modal", show("modal"))
+	router.POST("/floor", fowardJson())
 
 	router.Run()
 }
 
 func show(htmlPath string) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
+
 		list, eastList, westList := getTeamList()
 
-		fmt.Print(list)
+		log.Print(list)
 		ctx.HTML(200, htmlPath+".html", gin.H{
 			"allList":  list,
 			"eastList": eastList,
@@ -38,8 +43,91 @@ func show(htmlPath string) func(ctx *gin.Context) {
 
 func fowardJson() func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
-		ctx.JSON(200, "")
+
+		res, err := getData()
+		if err != nil {
+			log.Println(err.Error())
+			ctx.JSON(505, "")
+			return
+		}
+		log.Println(res)
+
+		bytes, err := json.Marshal(res)
+		if err != nil {
+			log.Println(err.Error())
+			ctx.JSON(505, string(bytes))
+		} else {
+			ctx.JSON(200, string(bytes))
+		}
+
 	}
+}
+
+func getList() []*dto.Tenanto {
+
+	auth := "1zBUevXg2gtvtgCaqw3n591l0PR949ZzgwXabjKm"
+	endpoint := "https://mac-001-1e4f9.firebaseio.com/"
+
+	c := firebase.NewClient(endpoint+"/billdiing/e_1", auth, nil)
+
+	retTenanto := func() interface{} {
+		return &dto.Tenanto{}
+	}
+
+	var list []*dto.Tenanto
+	for n := range c.Iterator(retTenanto) {
+		dto := n.Value.(*dto.Tenanto)
+		log.Println(dto)
+		list = append(list, dto)
+	}
+
+	return list
+}
+
+func getListJson(tenanto string) ([]*dto.Tenanto, error) {
+	bytes := useIoutilReadFile(tenanto)
+	var tenantoList []*dto.Tenanto
+	err := json.Unmarshal(bytes, &tenantoList)
+	if err != nil {
+		return nil, err
+	}
+	return tenantoList, nil
+}
+
+func getData() (map[string]interface{}, error) {
+	list, err := getListJson("e_1")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range list {
+
+		switch v.Acquisition {
+		case "A":
+			v.ClassName = "a_team"
+		case "B":
+			v.ClassName = "b_team"
+		case "C":
+			v.ClassName = "c_team"
+		default:
+			v.ClassName = "none-team"
+		}
+		log.Println(&v)
+	}
+
+	return map[string]interface{}{
+		"JSON":        "Data",
+		"tenantoList": list,
+	}, nil
+}
+
+func useIoutilReadFile(tenanto string) []byte {
+	bytes, err := ioutil.ReadFile("tool/json/" + tenanto + ".json")
+	if err != nil {
+		panic(err)
+	}
+
+	return bytes
 }
 
 func getTeamList() (allList, eastList, westList []*Team) {
@@ -71,7 +159,7 @@ func getTeamList() (allList, eastList, westList []*Team) {
 	allList = []*Team{A, B, C}
 
 	for _, v := range allList {
-		fmt.Println(v)
+		log.Println(v)
 	}
 
 	A = &Team{
