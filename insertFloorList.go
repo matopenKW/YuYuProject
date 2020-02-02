7f83b12822e69bee6ea2c0d8c5b8f83704162fa9
小野和論 <matopenkw@gmail.com>
2020-02-02 14:56:05 +0900

    Add insetSerial.go テナントのインサート文の作成

---
+++ insertFloorList.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"YuYuProject/internal/dao"
+	"YuYuProject/pkg/db"
+	"context"
+	"log"
+	"strconv"
+)
+
+func main() {
+
+	log.Println("start")
+
+	firestore, err := db.OpenFirestore()
+	if err != nil {
+		log.Fatalln("connection failed for CloudFireStore , error: %v", err)
+	}
+
+	log.Println(firestore)
+
+	tenantoDao := dao.GetTenatoDao()
+	floorList, err := tenantoDao("e_1")
+
+	for i, v := range floorList {
+		log.Println(v)
+
+		ctx := context.Background()
+		ref := firestore.Collection("test_building").Doc("twins").Collection("e_1")
+		doc := ref.Doc(strconv.Itoa(i + 1))
+
+		_, err := doc.Set(ctx, v)
+		if err != nil {
+			log.Fatalln("err")
+		}
+	}
+
+	log.Println("success")
+
+}
