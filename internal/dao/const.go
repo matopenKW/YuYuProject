2fc2e268d860d99ccac8c3b478a7b6c0ee486d75
matopenKW <matopenkw@gmail.com>
2020-02-11 15:58:36 +0900

    Add #21 JSON ・アップデート用のJSONファイルの追加 ・初期データインサート処理の追加

--- internal/dao/const.go
+++ internal/dao/const.go
@@ -1,3 +1,4 @@
 package dao
 
 const CONFIG_PATH = "pkg/conf/config.ini"
+const JSON_FOLDER_PATH = "tool/json/"
--- tool/insertFloorList.go
+++ tool/insertFloorList.go
@@ -4,37 +4,38 @@ import (
 	"YuYuProject/internal/dao"
 	"YuYuProject/pkg/db"
 	"context"
+	"errors"
 	"log"
 	"strconv"
 )
 
-func insertFloorList() {
+func InsertFloorList(flootId string) error {
 
 	log.Println("start")
 
 	firestore, err := db.OpenFirestore()
 	if err != nil {
-		log.Fatalln("connection failed for CloudFireStore , error: %v", err)
+		return errors.New("connection failed for CloudFireStore , error: " + err.Error())
 	}
 
-	log.Println(firestore)
-
 	tenantoDao := dao.GetTenatoDao()
-	floorList, err := tenantoDao("e_1")
+	floorList, err := tenantoDao(flootId)
 
 	for i, v := range floorList {
 		log.Println(v)
 
 		ctx := context.Background()
-		ref := firestore.Collection("test_building").Doc("twins").Collection("e_1")
+		ref := firestore.Collection("test_building").Doc("twins").Collection(flootId)
 		doc := ref.Doc(strconv.Itoa(i + 1))
 
 		_, err := doc.Set(ctx, v)
 		if err != nil {
-			log.Fatalln("err")
+			return err
 		}
 	}
 
 	log.Println("success")
+
+	return nil
 
 }
